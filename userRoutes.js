5d836f442a49f35bf3844f5b600979e55187da09
Samuel  adikah Mutesh <samsmollett50@gmail.com>
2025-10-25 23:09:14 +0300

    Fix import statement for userController functions

--- userRoutes.js
+++ userRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const multer = require('multer');
 const path = require('path');
-const {
+const { registerUser, loginUser } = require("../controllers/userController");  // Make sure these exist
   registerUser,
   loginUser,
   updateProfile
