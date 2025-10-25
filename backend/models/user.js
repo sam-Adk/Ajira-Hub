8b8d2e2d8967d0dc7ede24cf4c0765fc93c01fef
Samuel  adikah Mutesh <samsmollett50@gmail.com>
2025-10-25 23:27:14 +0300

    Refactor user schema to use object type definitions

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,14 +1,11 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  name: String,
+  name: { type: String },
   email: { type: String, unique: true },
-  password: String,
-  role: { type: String, enum: ['employer', 'househelp'] },
-  location: String,
-  skills: [String],
-  profilePic: String, // 🆕 URL to profile image
-  bio: String // 🆕 short description
+  password: { type: String },
+  bio: { type: String },
+  profilePic: { type: String },
 });
 
 module.exports = mongoose.model('User', userSchema);
